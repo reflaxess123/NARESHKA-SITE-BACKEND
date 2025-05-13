7da0a52865adcf8c1f9d128c0294b28d2a6b90a1
reflaxess123 <reflaxess@yandex.ru>
2025-05-13 12:26:25 +0300

    Обновлены настройки куки: свойство secure установлено в true для обеспечения безопасности, а sameSite изменено на "none" для поддержки кросс-доменных запросов.

--- src/index.ts
+++ src/index.ts
@@ -40,10 +40,10 @@ app.use(
     resave: false,
     saveUninitialized: false,
     cookie: {
-      secure: false, // В продакшене использовать secure cookies (HTTPS)
+      secure: true,
       httpOnly: true,
       maxAge: 1000 * 60 * 60 * 24, // 1 day
-      sameSite: "lax",
+      sameSite: "none",
     },
   })
 );
