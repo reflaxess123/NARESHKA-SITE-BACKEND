84e34fa47979f55b69d8094b7773c1cd07343862
reflaxess123 <reflaxess@yandex.ru>
2025-05-13 11:31:12 +0300

    Обновлены настройки CORS в приложении для разрешения запросов с http://localhost:5173 и добавлены необходимые параметры конфигурации.

--- src/index.ts
+++ src/index.ts
@@ -22,7 +22,15 @@ const redisStore = new RedisStore({
 });
 
 const app = express();
-app.use(cors());
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+};
+
+app.use(cors(corsOptions));
 const port = process.env.PORT || 3000;
 
 app.use(
