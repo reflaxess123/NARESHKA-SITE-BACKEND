ab186aced81cc882222893bb1cfefeb3cbbc1fb8
reflaxess123 <reflaxess@yandex.ru>
2025-05-13 12:41:13 +0300

    Обновлены настройки CORS для разрешения запросов с https://nareshka.site и http://localhost:5173, что улучшает поддержку кросс-доменных запросов.

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ const redisStore = new RedisStore({
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: ["https://nareshka.site", "http://localhost:5173"],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
